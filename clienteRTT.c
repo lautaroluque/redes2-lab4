db5743a58133b171fd3b5356c8b4e3f621ca8b8f
Lautaro Luque <lautaroluque@users.noreply.github.com>
2017-06-15 18:11:58 -0300

    renegando

--- clienteRTT.c
+++ clienteRTT.c
@@ -1,156 +1,191 @@
 #include "mensajesRTT.h"
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
 
-	if(argc!=3){
-		syntax("clienteRTT");
-	}
+    if(argc!=3)
+    {
+        syntax("clienteRTT");
+    }
 
-	char * ip = argv[1];
-        unsigned short prt = atoi(argv[2]);
-	int sckt;
+    char * ip = argv[1];
+    unsigned short prt = atoi(argv[2]);
+    int sckt;
 
-        if((sckt = socket(PF_INET, SOCK_STREAM, 0)) < 0){
-                fatale("No se pudo crear el socket");
+    if((sckt = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        fatale("No se pudo crear el socket");
+    }
+
+    struct sockaddr_in srvr = { .sin_family = AF_INET, .sin_port = htons(prt), .sin_addr.s_addr = inet_addr(ip)};
+
+    if(connect(sckt, (struct sockaddr *) &srvr, sizeof(srvr)) < 0)
+    {
+        fatale("Fallo en el connect");
+    }
+
+    char rspst[BUFSIZE];
+    char mnsj[BUFSIZE];
+    int tmrspst;
+
+    printf("Conexion correcta\n");
+
+    bzero(rspst, BUFSIZE);
+    if((tmrspst = recv(sckt, rspst, BUFSIZE, 0)) < 0)
+    {
+        printf("Error en la recepcion\n");
+    }
+
+    printf("%s\n", rspst);
+
+    if(strcmp(rspst, MSG_220) == 0)
+    {
+        printf("Ingrese su usuario: ");
+        char * usr = NULL;
+        size_t lngtd = 0;
+        if(getline(&usr, &lngtd, stdin) > 0)
+        {
+            bzero(mnsj, BUFSIZE);
+            strcpy(mnsj, "USER ");
+            strcat(mnsj, usr);
+            send(sckt, mnsj, BUFSIZE, 0);
         }
+    }
 
-        struct sockaddr_in srvr = { .sin_family = AF_INET, .sin_port = htons(prt), .sin_addr.s_addr = inet_addr(ip)};
+    bzero(rspst, BUFSIZE);
+    if((tmrspst = recv(sckt, rspst, BUFSIZE, 0)) < 0)
+    {
+        printf("Error en la recepcion\n");
+    }
 
-        if(connect(sckt, (struct sockaddr *) &srvr, sizeof(srvr)) < 0){
-                fatale("Fallo en el connect");
+    printf("%s\n", rspst);
+
+    if(strncmp(rspst, "331 ", 4) == 0)
+    {
+        printf("Ingrese su password: ");
+        char * psswrd = NULL;
+        size_t lngtd = 0;
+        if(getline(&psswrd, &lngtd, stdin) > 0)
+        {
+            bzero(mnsj, BUFSIZE);
+            strcpy(mnsj, "PASS ");
+            strcat(mnsj, psswrd);
+            send(sckt, mnsj, BUFSIZE, 0);
         }
+    }
 
-	char rspst[BUFSIZE];
-	char mnsj[BUFSIZE];
-	int tmrspst;
+    bzero(rspst, BUFSIZE);
+    if((tmrspst = recv(sckt, rspst, BUFSIZE, 0)) < 0)
+    {
+        printf("Error en la recepcion\n");
+    }
 
-	printf("Conexion correcta\n");
+    printf("%s\n", rspst);
 
-	bzero(rspst, BUFSIZE);
-	if((tmrspst = recv(sckt, rspst, BUFSIZE, 0)) < 0){
-		printf("Error en la recepcion\n");
-	}
+    if(strncmp(rspst, "530 ", 4) == 0)
+    {
+        close(sckt);
+        return 0;
+    }
 
-	printf("%s\n", rspst);
+    if(strncmp(rspst, "230 ", 4) == 0)
+    {
+        while(sckt)
+        {
+            printf("Operacion:");
+            char * prcn = NULL;
+            char * nmbrrchv = NULL;
+            size_t lngtd = 0;
+            if(getline(&prcn, &lngtd, stdin) > 0)
+            {
+                if(strncmp(prcn, "get ",4) == 0)
+                {
+                    if((strlen(prcn)) < 5)
+                    {
+                        printf("Nombre de archivo invalido");
+                        break;
+                    }
+                    else
+                    {
+                        nmbrrchv = malloc(strlen(prcn)+1);
+                        strcpy(nmbrrchv, prcn);
+                        nmbrrchv += 4;
+                        for(int i = 0; i <= strlen(nmbrrchv); i++)
+                        {
+                            if(isspace(nmbrrchv[i]) != 0)
+                            {
+                                nmbrrchv[i] = '\0';
+                                break;
+                            }
+                        }
+                    }
+                    bzero(mnsj, BUFSIZE);
+                    strcpy(mnsj, "RETR ");
+                    strcat(mnsj, nmbrrchv);
+                    send(sckt, mnsj, BUFSIZE, 0);
+                }
+            }
 
-	if(strcmp(rspst, MSG_220) == 0){
-		printf("Ingrese su usuario: ");
-		char * usr = NULL;
-		size_t lngtd = 0;
-		if(getline(&usr, &lngtd, stdin) > 0){
-			bzero(mnsj, BUFSIZE);
-			strcpy(mnsj, "USER ");
-			strcat(mnsj, usr);
-			send(sckt, mnsj, BUFSIZE, 0);
-		}
-	}
+            bzero(rspst, BUFSIZE);
+            if((tmrspst = recv(sckt, rspst, BUFSIZE, 0)) < 0)
+            {
+                printf("Error en la recepcion\n");
+            }
 
-	bzero(rspst, BUFSIZE);
-	if((tmrspst = recv(sckt, rspst, BUFSIZE, 0)) < 0){
-		printf("Error en la recepcion\n");
-	}
+            if(strncmp(rspst, "299 ", 4) == 0)
+            {
+                printf("%s\n", rspst);
+                //char * strtmnrchv = malloc(strlen(rspst)+1);
+                int tmnrchv = 0;
+                //strcpy(strtmnrchv, rspst);
+                //strtmnrchv += 9;
+                //strtmnrchv += (strlen(nmbrrchv));
+                //strtmnrchv += 5;
+                //printf("%s", strtmnrchv);
+                //for(int i = 0; i <= strlen(strtmnrchv); i++){
+                //	if(isspace(strtmnrchv[i]) != 0){
+                //		strtmnrchv[i] = '\0';
+                //		break;
+                //	}
+                //}
+                //printf("%s", strtmnrchv);
 
-	printf("%s\n", rspst);
+                if((tmrspst = recv(sckt, &tmnrchv, sizeof(int), 0)) < 0)
+                {
+                    printf("Error en la recepcion\n");
+                }
 
-	if(strncmp(rspst, "331 ", 4) == 0){
-		printf("Ingrese su password: ");
-		char * psswrd = NULL;
-		size_t lngtd = 0;
-		if(getline(&psswrd, &lngtd, stdin) > 0){
-			bzero(mnsj, BUFSIZE);
-			strcpy(mnsj, "PASS ");
-			strcat(mnsj, psswrd);
-			send(sckt, mnsj, BUFSIZE, 0);
-		}
-	}
+                //tmnrchv = atoi(strtmnrchv);
 
-	bzero(rspst, BUFSIZE);
-	if((tmrspst = recv(sckt, rspst, BUFSIZE, 0)) < 0){
-		printf("Error en la recepcion\n");
-	}
+                FILE * rchv;
+                rchv = fopen(nmbrrchv, "w");
 
-	printf("%s\n", rspst);
+                int trfr = tmnrchv;
 
-	if(strncmp(rspst, "530 ", 4) == 0){
-		close(sckt);
-		return 0;
-	}
+                bzero(rspst, BUFSIZE);
+                while(((tmrspst = recv(sckt, rspst, BUFSIZE, 0)) > 0) && (trfr > 0))
+                {
+                    fwrite(rspst, sizeof(char), tmrspst, rchv);
+                    trfr -= tmrspst;
+                    printf("Recibiendo archivo");
+                }
 
-	if(strncmp(rspst, "230 ", 4) == 0){
-		while(sckt){
-			printf("Operacion:");
-			char * prcn = NULL;
-			char * nmbrrchv = NULL;
-			size_t lngtd = 0;
-			if(getline(&prcn, &lngtd, stdin) > 0){
-				if(strncmp(prcn, "get ",4) == 0){
-					if((strlen(prcn)) < 5){
-						printf("Nombre de archivo invalido");
-						break;
-					}else{
-						nmbrrchv = malloc(strlen(prcn)+1);
-						strcpy(nmbrrchv, prcn);
-						nmbrrchv += 4;
-					}
-					bzero(mnsj, BUFSIZE);
-					strcpy(mnsj, "RETR ");
-					strcat(mnsj, nmbrrchv);
-					send(sckt, mnsj, BUFSIZE, 0);
-				}
-			}
+                bzero(rspst, BUFSIZE);
+                if((tmrspst = recv(sckt, rspst, BUFSIZE, 0)) < 0)
+                {
+                    printf("Error en la recepcion\n");
+                }
+                printf("%s\n", rspst);
+                break;
+            }
+            else if(strncmp(rspst, "550 ", 4) == 0)
+            {
+                printf("%s\n", rspst);
+                break;
+            }
 
-			bzero(rspst, BUFSIZE);
-			if((tmrspst = recv(sckt, rspst, BUFSIZE, 0)) < 0){
-				printf("Error en la recepcion\n");
-			}
+        }
+    }
 
-			if(strncmp(rspst, "299 ", 4) == 0){
-				printf("%s\n", rspst);
-				//char * strtmnrchv = malloc(strlen(rspst)+1);
-				int tmnrchv = 0;
-				//strcpy(strtmnrchv, rspst);
-				//strtmnrchv += 9;
-				//strtmnrchv += (strlen(nmbrrchv));
-				//strtmnrchv += 5;
-				//printf("%s", strtmnrchv);
-				//for(int i = 0; i <= strlen(strtmnrchv); i++){
-				//	if(isspace(strtmnrchv[i]) != 0){
-				//		strtmnrchv[i] = '\0';
-				//		break;
-				//	}
-				//}
-				//printf("%s", strtmnrchv);
-
-				if((tmrspst = recv(sckt, &tmnrchv, sizeof(int), 0)) < 0){
-					printf("Error en la recepcion\n");
-				}
-
-				//tmnrchv = atoi(strtmnrchv);
-
-				FILE * rchv;
-				rchv = fopen(nmbrrchv, "w");
-
-				int trfr = tmnrchv;
-
-				bzero(rspst, BUFSIZE);
-				while(((tmrspst = recv(sckt, rspst, BUFSIZE, 0)) > 0) && (trfr > 0)){
-					fwrite(rspst, sizeof(char), tmrspst, rchv);
-					trfr -= tmrspst;
-				}
-
-				bzero(rspst, BUFSIZE);
-				if((tmrspst = recv(sckt, rspst, BUFSIZE, 0)) < 0){
-					printf("Error en la recepcion\n");
-				}
-				printf("%s\n", rspst);
-				break;
-			}else if(strncmp(rspst, "550 ", 4) == 0){
-				printf("%s\n", rspst);
-				break;
-			}
-
-		}
-	}
-
-	return 0;
+    return 0;
 }
--- servidorRTT.c
+++ servidorRTT.c
@@ -1,193 +1,233 @@
 #include "mensajesRTT.h"
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
 
-	if(argc!=2){
-		printf("Uso: servidorRTT [puerto]\n");
-	}
+    if(argc!=2)
+    {
+        printf("Uso: servidorRTT [puerto]\n");
+    }
 
-	unsigned short puerto = atoi(argv[1]);
-	int socketescucha;
-	int socketconexion;
+    unsigned short puerto = atoi(argv[1]);
+    int socketescucha;
+    int socketconexion;
 
-	if((socketescucha = socket(PF_INET, SOCK_STREAM, 0)) < 0){
-		fatale("No se pudo crear el socket");
-	}
+    if((socketescucha = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        fatale("No se pudo crear el socket");
+    }
 
-	struct sockaddr_in server = { .sin_family = AF_INET, .sin_addr.s_addr = htonl(INADDR_ANY), .sin_port = htons(puerto)};
+    struct sockaddr_in server = { .sin_family = AF_INET, .sin_addr.s_addr = htonl(INADDR_ANY), .sin_port = htons(puerto)};
 
-	if(bind(socketescucha, (struct sockaddr *) &server, sizeof(server)) < 0){
-		fatale("Fallo en el bind");
-	}
+    if(bind(socketescucha, (struct sockaddr *) &server, sizeof(server)) < 0)
+    {
+        fatale("Fallo en el bind");
+    }
 
-	listen(socketescucha, 1);
-	for(;;){
+    listen(socketescucha, 1);
+    for(;;)
+    {
 
-		struct sockaddr_in cliente;
-		socklen_t tamanocliente = sizeof(cliente);
+        struct sockaddr_in cliente;
+        socklen_t tamanocliente = sizeof(cliente);
 
-		socketconexion = accept(socketescucha, (struct sockaddr *)&cliente, &tamanocliente);
+        socketconexion = accept(socketescucha, (struct sockaddr *)&cliente, &tamanocliente);
 
-		if(socketconexion < 0){
-			fatale("Fallo en el accept");
-		}
+        if(socketconexion < 0)
+        {
+            fatale("Fallo en el accept");
+        }
 
-		while(socketconexion){
-			char respuesta[BUFSIZE];
-			char mensaje[BUFSIZE];
-			int tamanomensaje;
-			int tamanorespuesta;
+        while(socketconexion)
+        {
+            char respuesta[BUFSIZE];
+            char mensaje[BUFSIZE];
+            int tamanomensaje;
+            int tamanorespuesta;
 
-			char * usuario = malloc(BUFSIZE);
-			char * contrasena = malloc(BUFSIZE);
-			char * combo = malloc(BUFSIZE);
-			FILE * lista;
-			int login = 0;
+            char * usuario = malloc(BUFSIZE);
+            char * contrasena = malloc(BUFSIZE);
+            char * combo = malloc(BUFSIZE);
+            FILE * lista;
+            int login = 0;
 
-       		 	bzero(respuesta, BUFSIZE);
-        		bzero(mensaje, BUFSIZE);
-			strcpy(mensaje, MSG_220);
-			if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0){
-				printf("Error en el envio\n");
-				break;
-			}
-			if((tamanorespuesta = recv(socketconexion, respuesta, BUFSIZE, 0)) < 0){
-				printf("Error en la respuesta\n");
-				break;
-			}
+            bzero(respuesta, BUFSIZE);
+            bzero(mensaje, BUFSIZE);
+            strcpy(mensaje, MSG_220);
+            if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0)
+            {
+                printf("Error en el envio\n");
+                break;
+            }
+            if((tamanorespuesta = recv(socketconexion, respuesta, BUFSIZE, 0)) < 0)
+            {
+                printf("Error en la respuesta\n");
+                break;
+            }
 
-			if(strncmp(respuesta, "USER ", 5) == 0){
-				strcpy(usuario, respuesta);
-				usuario += 5;
-				for(int i = 0; i <= strlen(usuario); i++){
-					if(isspace(usuario[i]) != 0){
-						usuario[i] = '\0';
-						break;
-					}
-				}
-			}
+            if(strncmp(respuesta, "USER ", 5) == 0)
+            {
+                strcpy(usuario, respuesta);
+                usuario += 5;
+                for(int i = 0; i <= strlen(usuario); i++)
+                {
+                    if(isspace(usuario[i]) != 0)
+                    {
+                        usuario[i] = '\0';
+                        break;
+                    }
+                }
+            }
 
-			bzero(respuesta, BUFSIZE);
-	        	bzero(mensaje, BUFSIZE);
-			sprintf(mensaje, MSG_331, usuario);
-			if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0){
-				printf("Error en el envio\n");
-				break;
-			}
-			if((tamanorespuesta = recv(socketconexion, respuesta, BUFSIZE, 0)) < 0){
-				printf("Error en la respuesta\n");
-				break;
-			}
+            bzero(respuesta, BUFSIZE);
+            bzero(mensaje, BUFSIZE);
+            sprintf(mensaje, MSG_331, usuario);
+            if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0)
+            {
+                printf("Error en el envio\n");
+                break;
+            }
+            if((tamanorespuesta = recv(socketconexion, respuesta, BUFSIZE, 0)) < 0)
+            {
+                printf("Error en la respuesta\n");
+                break;
+            }
 
-			if(strncmp(respuesta, "PASS ", 5) == 0){
-				strcpy(contrasena, respuesta);
-				contrasena += 5;
-			}else{
-				printf("Respuesta invalida: password\n");
-				break;
-			}
+            if(strncmp(respuesta, "PASS ", 5) == 0)
+            {
+                strcpy(contrasena, respuesta);
+                contrasena += 5;
+            }
+            else
+            {
+                printf("Respuesta invalida: password\n");
+                break;
+            }
 
-			strcat(combo, usuario);
-			strcat(combo, ":");
-			strcat(combo, contrasena);
+            strcat(combo, usuario);
+            strcat(combo, ":");
+            strcat(combo, contrasena);
 
-			lista = fopen(USRFILE, "r");
-			if(lista){
-				int leido;
-				size_t largolinea = 0;
-				char * linea = NULL;
-				while((leido = getline(&linea, &largolinea, lista)) != -1){
-					if((strcmp(combo, linea)) != 0){
-						continue;
-					}
-					else{
-						login = 1;
-						break;
-					}
-				}
-			}
+            lista = fopen(USRFILE, "r");
+            if(lista)
+            {
+                int leido;
+                size_t largolinea = 0;
+                char * linea = NULL;
+                while((leido = getline(&linea, &largolinea, lista)) != -1)
+                {
+                    if((strcmp(combo, linea)) != 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        login = 1;
+                        break;
+                    }
+                }
+            }
 
-			fclose(lista);
+            fclose(lista);
 
-			if(login == 1){
-				bzero(respuesta, BUFSIZE);
-				bzero(mensaje, BUFSIZE);
-				sprintf(mensaje, MSG_230, usuario);
-				if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0){
-					printf("Error en el envio\n");
-					break;
-				}
-				if((tamanorespuesta = recv(socketconexion, respuesta, BUFSIZE, 0)) < 0){
-					printf("Error en la respuesta\n");
-					break;
-				}
-				if(strncmp(respuesta, "RETR ", 5) == 0){
-					char * nombrearchivo = malloc(strlen(respuesta)+1);
-					strcpy(nombrearchivo, respuesta);
-					nombrearchivo += 5;
-					for(int i = 0; i <= strlen(nombrearchivo); i++){
-						if(isspace(nombrearchivo[i]) != 0){
-							nombrearchivo[i] = '\0';
-							break;
-						}
-					}
+            if(login == 1)
+            {
+                bzero(respuesta, BUFSIZE);
+                bzero(mensaje, BUFSIZE);
+                sprintf(mensaje, MSG_230, usuario);
+                if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0)
+                {
+                    printf("Error en el envio\n");
+                    break;
+                }
+                if((tamanorespuesta = recv(socketconexion, respuesta, BUFSIZE, 0)) < 0)
+                {
+                    printf("Error en la respuesta\n");
+                    break;
+                }
+                if(strncmp(respuesta, "RETR ", 5) == 0)
+                {
+                    char * nombrearchivo = malloc(strlen(respuesta)+1);
+                    strcpy(nombrearchivo, respuesta);
+                    nombrearchivo += 5;
+                    for(int i = 0; i <= strlen(nombrearchivo); i++)
+                    {
+                        if(isspace(nombrearchivo[i]) != 0)
+                        {
+                            nombrearchivo[i] = '\0';
+                            break;
+                        }
+                    }
 
-					FILE * archivo;
-					archivo = fopen(nombrearchivo, "r");
+                    FILE * archivo;
+                    archivo = fopen(nombrearchivo, "r");
 
-					if(archivo){
-						int tamanoarchivo;
-						fseek(archivo, 0, SEEK_END);
-						tamanoarchivo = ftell(archivo);
-						rewind(archivo);
+                    if(archivo)
+                    {
+                        int tamanoarchivo;
+                        fseek(archivo, 0, SEEK_END);
+                        tamanoarchivo = ftell(archivo);
+                        rewind(archivo);
 
-						bzero(respuesta, BUFSIZE);
-						bzero(mensaje, BUFSIZE);
-						sprintf(mensaje, MSG_299, nombrearchivo, tamanoarchivo);
-						if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0){
-							printf("Error en el envio 299\n");
-							break;
-						}
+                        bzero(respuesta, BUFSIZE);
+                        bzero(mensaje, BUFSIZE);
+                        sprintf(mensaje, MSG_299, nombrearchivo, tamanoarchivo);
+                        if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0)
+                        {
+                            printf("Error en el envio 299\n");
+                            break;
+                        }
 
-						if((tamanomensaje = send(socketconexion, &tamanoarchivo, sizeof(int), 0)) < 0){
-							printf("Error en el envio filesize\n");
-							break;
-						}
+                        if((tamanomensaje = send(socketconexion, &tamanoarchivo, sizeof(int), 0)) < 0)
+                        {
+                            printf("Error en el envio filesize\n");
+                            break;
+                        }
 
-						bzero(mensaje, BUFSIZE);
-						while(fread(mensaje, BUFSIZE, 1, archivo) == BUFSIZE){
-							if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0){
-								printf("Error en el envio archivo\n");
-								break;
-							}
-						}
+                        int transfer = 0;
+                        bzero(mensaje, BUFSIZE);
+                        /*while(fread(mensaje, BUFSIZE, 1, archivo) == BUFSIZE)
+                        {
+                            if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0)
+                            {
+                                printf("Error en el envio archivo\n");
+                                break;
+                            }
+                        }*/
 
-						bzero(mensaje, BUFSIZE);
-						sprintf(mensaje, MSG_226);
-						if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0){
-							printf("Error en el envio 226\n");
-							break;
-						}
-					}else{
-						bzero(mensaje, BUFSIZE);
-						sprintf(mensaje, MSG_550, nombrearchivo);
-						if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0){
-							printf("Error en el envio\n");
-							break;
-						}
-					}
-				}
-			}else if(login == 0){
-				bzero(respuesta, BUFSIZE);
-				bzero(mensaje, BUFSIZE);
-				sprintf(mensaje, MSG_530);
-				if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0){
-					printf("Error en el envio\n");
-					break;
-				}
-				close(socketconexion);
-			}
+                        bzero(mensaje, BUFSIZE);
+                        sprintf(mensaje, MSG_226);
+                        if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0)
+                        {
+                            printf("Error en el envio 226\n");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        bzero(mensaje, BUFSIZE);
+                        sprintf(mensaje, MSG_550, nombrearchivo);
+                        if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0)
+                        {
+                            printf("Error en el envio\n");
+                            break;
+                        }
+                    }
+                }
+            }
+            else if(login == 0)
+            {
+                bzero(respuesta, BUFSIZE);
+                bzero(mensaje, BUFSIZE);
+                sprintf(mensaje, MSG_530);
+                if((tamanomensaje = send(socketconexion, mensaje, BUFSIZE, 0)) < 0)
+                {
+                    printf("Error en el envio\n");
+                    break;
+                }
+                close(socketconexion);
+            }
 
-		}
-	}
+        }
+    }
 }
